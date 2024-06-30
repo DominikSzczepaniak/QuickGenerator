eed0862eb002ff0d51c22f8f50dd21e556a889bf
dominik <szczepaniakdominik5@gmail.com>
2024-06-30 21:24:27 +0200

    Changed a bit looks of website, done TODO5

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,23 @@ interface CategoryProps {
   handleDraw: () => [string, string];
 }
 
+//TODO
+// 1. Add conditionals to drawing
+// 2. Make reading the website easier
+// 3. Change style of it, add dark theme
+// 4. Make tests
+// 5. Allow user to reroll, dont just reset
+
 function App() {
   const [categories, setCategories] = useState<CategoryProps[]>([]);
   const [drawCompleted, setDrawCompleted] = useState(false);
   const [results, setResults] = useState<string[][]>([]);
-  let handleDrawFunctions = useRef<{id: number, handleDraw: () => [string, string]}[]>([]).current;
-  let saveDataFunctions = useRef<{id: number, saveData: () => {name: string, drawings: {ppbValue: string, countValue: string, inputValue: string}[]}}[]>([]).current;
-  let loadDataFunctions = useRef<{id: number, loadData: (data: {name: string, drawings: {ppbValue: string, countValue: string, inputValue: string}[]}) => void}[]>([]).current;
+  let handleDrawFunctions = useRef<{ id: number, handleDraw: () => [string, string] }[]>([]).current;
+  let saveDataFunctions = useRef<{ id: number, saveData: () => { name: string, drawings: { ppbValue: string, countValue: string, inputValue: string }[] } }[]>([]).current;
+  let loadDataFunctions = useRef<{ id: number, loadData: (data: { name: string, drawings: { ppbValue: string, countValue: string, inputValue: string }[] }) => void }[]>([]).current;
 
   const addCategory = () => {
-    setCategories([...categories, { id: categories.length, name: "Type the name of this category...", handleDraw: () => ["", ""]}]);
+    setCategories([...categories, { id: categories.length, name: "Type the name of this category...", handleDraw: () => ["", ""] }]);
   };
 
   function drawAnswers(): [string, string][] {
@@ -27,19 +34,19 @@ function App() {
 
   const registerHandleDraw = (id: number, handleDraw: () => [string, string]) => {
     let current = handleDrawFunctions.filter(fn => fn.id !== id);
-    current.push({id, handleDraw});
+    current.push({ id, handleDraw });
     handleDrawFunctions = current;
   }
 
-  const registerSaveData = (id: number, saveData: () => {name: string, drawings: {ppbValue: string, countValue: string, inputValue: string}[]}) => {
+  const registerSaveData = (id: number, saveData: () => { name: string, drawings: { ppbValue: string, countValue: string, inputValue: string }[] }) => {
     let current = saveDataFunctions.filter(fn => fn.id !== id);
-    current.push({id, saveData});
+    current.push({ id, saveData });
     saveDataFunctions = current;
   }
 
-  const registerLoadData = (id: number, loadData: (data: {name: string, drawings: {ppbValue: string, countValue: string, inputValue: string}[]}) => void) => {
+  const registerLoadData = (id: number, loadData: (data: { name: string, drawings: { ppbValue: string, countValue: string, inputValue: string }[] }) => void) => {
     let current = loadDataFunctions.filter(fn => fn.id !== id);
-    current.push({id, loadData});
+    current.push({ id, loadData });
     loadDataFunctions = current;
   }
 
@@ -53,14 +60,14 @@ function App() {
 
   const saveInfo = () => {
     let data = saveDataFunctions.map(fn => fn.saveData());
-    let dataString = JSON.stringify(data, null, 2); 
+    let dataString = JSON.stringify(data, null, 2);
 
     const blob = new Blob([dataString], { type: 'application/json' });
     const url = URL.createObjectURL(blob);
 
     const a = document.createElement('a');
     a.href = url;
-    a.download = 'data.json'; 
+    a.download = 'data.json';
     a.click();
 
     URL.revokeObjectURL(url);
@@ -82,9 +89,9 @@ function App() {
     }
   };
 
-  const loadData = (data: {name: string, drawings: {ppbValue: string, countValue: string, inputValue: string}[]}[]) => {
+  const loadData = (data: { name: string, drawings: { ppbValue: string, countValue: string, inputValue: string }[] }[]) => {
     const newCategories = data.map((category, index) => ({
-      id: index,  
+      id: index,
       name: category.name,
       handleDraw: () => ["", ""] as [string, string],
       drawings: []
@@ -103,37 +110,6 @@ function App() {
     <div>
       {/* <Navbar /> */}
       <div className="p-4">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={addCategory}
-        >
-          Add category
-        </button>
-        <button
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-          onClick={handleDrawClick}
-        >
-          Draw
-        </button>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => saveInfo()}
-        >
-          Save
-        </button>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => document.getElementById('fileInput')?.click()}
-        >
-          Load
-        </button>
-        <input
-          id="fileInput"
-          type="file"
-          accept=".json"
-          onChange={handleFileUpload}
-          style={{ display: 'none' }}
-        />
         {drawCompleted ? (
           <div>
             <h2 className="text-2xl font-bold mb-4">Summary of Results</h2>
@@ -148,22 +124,55 @@ function App() {
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
               onClick={() => setDrawCompleted(false)}
             >
-              Reset
+              Go back and lose result of this draw
             </button>
           </div>
         ) : (
-          categories.map((category) => (
-            <Category
-              key={category.id}
-              id={category.id}
-              deleteCategory={(id: number) => {
-                setCategories(categories.filter((category) => category.id !== id));
-              }}
-              registerHandleDraw={registerHandleDraw}
-              registerSaveData={registerSaveData}
-              registerLoadData={registerLoadData}
+          <div>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={addCategory}
+            >
+              Add category
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => saveInfo()}
+            >
+              Save
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => document.getElementById('fileInput')?.click()}
+            >
+              Load
+            </button>
+            <input
+              id="fileInput"
+              type="file"
+              accept=".json"
+              onChange={handleFileUpload}
+              style={{ display: 'none' }}
             />
-          ))
+            <button
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+              onClick={handleDrawClick}
+            >
+              Draw
+            </button>
+            {categories.map((category) => (
+              <Category
+                key={category.id}
+                id={category.id}
+                deleteCategory={(id: number) => {
+                  setCategories(categories.filter((category) => category.id !== id));
+                }}
+                registerHandleDraw={registerHandleDraw}
+                registerSaveData={registerSaveData}
+                registerLoadData={registerLoadData}
+              />
+            ))}
+          </div>
         )}
       </div>
     </div>
